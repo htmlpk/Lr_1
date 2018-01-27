05481407e9642cb8c9aa651d4a4b6df5b573e379
htmlpk <htmlpk@ukr.net>
2018-01-27 15:11:13 +0200

    Add files via upload

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/multiply.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    interface multiply
+    {
+        double stepen(double x);//степень
+        double abs(double x);//модуль
+        double tan();
+        double ctg();
+        double log(double x, int osnovanie);//логарфм с основанием
+        double fac(double x);//факториал
+
+    }
+}
